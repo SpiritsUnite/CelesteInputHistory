febaa8ba4093b1cca026776a9ad3be4541bab220
Austin Tankiang <austi.tanki@gmail.com>
2021-10-17 20:35:02 +1100

    Support crouch dash and multiple jump binds

--- InputHistory/HistoryEvent.cs
+++ InputHistory/HistoryEvent.cs
@@ -17,20 +17,41 @@ namespace Celeste.Mod.InputHistory
         public long Frames { get; set; }
         private int MoveX;
         private int MoveY;
-        private bool Jump;
+        private int Jump;
         private bool Dash;
+        private bool CrouchDash;
         private bool Grab;
         private bool LevelLoad;
+
+        private int CheckCount(VirtualButton button)
+        {
+            int ret = 0;
+            foreach (var node in button.Nodes)
+            {
+                if (node.Check) ret += 1;
+            }
+            foreach (var key in button.Binding.Keyboard)
+            {
+                if (MInput.Keyboard.Check(key)) ret += 1;
+            }
+            foreach (var padButton in button.Binding.Controller)
+            {
+                if (MInput.GamePads[button.GamepadIndex].Check(padButton, button.Threshold))
+                    ret += 1;
+            }
+            return ret;
+        }
 
         public HistoryEvent()
         {
             Time = Engine.RawDeltaTime;
             Frames = 1;
-            MoveX = Input.MoveX;
-            MoveY = Input.MoveY;
-            Jump = Input.Jump.Check;
-            Dash = Input.Dash.Check;
-            Grab = Input.Grab.Check;
+            MoveX = (int)Input.MoveX;
+            MoveY = (int)Input.MoveY;
+            Jump = CheckCount(Input.Jump);
+            Dash = Input.Dash;
+            CrouchDash = Input.CrouchDash;
+            Grab = Input.Grab;
             LevelLoad = false;
         }
 
@@ -55,12 +76,29 @@ namespace Celeste.Mod.InputHistory
             var rightDir = Input.GuiDirection(new Vector2(1, 0));
             x += rightDir.Width * fontSize / rightDir.Height;
 
-            var jump1 = Input.GuiKey(Microsoft.Xna.Framework.Input.Keys.J);
-            if (Jump) jump1.Draw(new Vector2(x, y), Vector2.Zero, Color.White, fontSize / jump1.Height);
-            x += jump1.Width * fontSize / jump1.Height;
+            var jump = Input.GuiKey(Microsoft.Xna.Framework.Input.Keys.J);
+            for (int i = 0; i < Jump; i++)
+            {
+                float multiScale = 5f / (5 + Jump - 1);
+                var shift = new Vector2(jump.Width * fontSize / jump.Height * (i / (5f + Jump - 1)),
+                    fontSize * ((Jump - i - 1) / (5f + Jump - 1)));
+                jump.Draw(new Vector2(x, y) + shift, Vector2.Zero, Color.White, fontSize / jump.Height * multiScale);;
+            }
+            x += jump.Width * fontSize / jump.Height;
 
             var dash = Input.GuiKey(Microsoft.Xna.Framework.Input.Keys.X);
-            if (Dash) dash.Draw(new Vector2(x, y), Vector2.Zero, Color.White, fontSize / dash.Height);
+            var crouchDash = Input.GuiKey(Microsoft.Xna.Framework.Input.Keys.Z);
+            float dashScale = 1f;
+            float dashShift = 0f;
+            float crouchDashShift = 0f;
+            if (Dash && CrouchDash)
+            {
+                dashScale = 5 / 6f;
+                dashShift = fontSize / 6f;
+                crouchDashShift = crouchDash.Width * fontSize / crouchDash.Height / 6;
+            }
+            if (Dash) dash.Draw(new Vector2(x, y + dashShift), Vector2.Zero, Color.White, fontSize / dash.Height * dashScale);
+            if (CrouchDash) crouchDash.Draw(new Vector2(x + crouchDashShift, y), Vector2.Zero, Color.White, fontSize / dash.Height * dashScale);
             x += dash.Width * fontSize / dash.Height;
 
             var grab = Input.GuiKey(Microsoft.Xna.Framework.Input.Keys.G);
@@ -83,6 +121,7 @@ namespace Celeste.Mod.InputHistory
                 MoveY == other.MoveY &&
                 Jump == other.Jump &&
                 Dash == other.Dash &&
+                CrouchDash == other.CrouchDash &&
                 Grab == other.Grab &&
                 LevelLoad == other.LevelLoad);
         }
@@ -95,8 +134,9 @@ namespace Celeste.Mod.InputHistory
             if (MoveX == -1) ret += "Left ";
             if (MoveY == 1) ret += "Up ";
             if (MoveY == -1) ret += "Down ";
-            if (Jump) ret += "Jump ";
+            if (Jump > 0) ret += "Jump " + Jump.ToString() + " ";
             if (Dash) ret += "Dash ";
+            if (CrouchDash) ret += "Crouch Dash ";
             if (LevelLoad) ret += "Level Load";
             return ret;
         }
